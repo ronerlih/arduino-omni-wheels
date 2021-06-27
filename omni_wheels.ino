99935f6732d44bc99ef32db92b70a90f8738d2d9
ron erlih <ronerlih@gmail.com>
2021-06-27 16:43:12 -0400

    motor class & methods, 6 main car movements

--- omni_wheels.ino
+++ omni_wheels.ino
@@ -1,217 +1,137 @@
-#include <Wire.h>
-#include <SoftwareSerial.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-#define MAXCHAR 30
+class Motor {
+  private:
+    int ena;
+    int in1;
+    int in2;
+  public:
+    Motor(int ena, int in1, int in2);
+    void setup();
+    void moveForward();
+    void moveBackward();
+    void setSpeed(int speed);
+    void stop();
+};
+Motor::Motor(int ena, int in1, int in2) {
+  this->ena = ena;
+  this->in1 = in1;
+  this->in2 = in2;
 
-SoftwareSerial Bluetooth(2, 3); // Arduino(RX, TX) - HC-05 Bluetooth (TX, RX)
+}
+void Motor::moveForward() {
+  digitalWrite(this->in1, HIGH);
+  digitalWrite(this->in2, LOW);
+};
+void Motor::moveBackward() {
+  digitalWrite(this->in1, LOW);
+  digitalWrite(this->in2, HIGH);
+};
+void Motor::setSpeed(int speed) {
+  analogWrite(this->ena, speed);
+};
+void Motor::stop() {
+  digitalWrite(this->in1, LOW);
+  digitalWrite(this->in2, LOW);
+};
+void Motor::setup() {
+  pinMode(this->ena, OUTPUT);
+  pinMode(this->in1, OUTPUT);
+  pinMode(this->in2, OUTPUT);
+};
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Motor bottomLeft(6, 5, 7);
+Motor bottomRight(3, 2, 4);
+Motor topLeft(11, 13, 12);
+Motor topRight(9, 10, 8);
 
-Adafruit_DCMotor *LeftFrontWheel  = AFMS.getMotor(1);
-Adafruit_DCMotor *RightFrontWheel   = AFMS.getMotor(2); 
-Adafruit_DCMotor *LeftBackWheel = AFMS.getMotor(3);
-Adafruit_DCMotor *RightBackWheel  = AFMS.getMotor(4);
+void setup()
+{
+  bottomLeft.setup();
+  bottomRight.setup();
+  topLeft.setup();
+  topRight.setup();
 
-int defaultSpeed = 60;
-char message[MAXCHAR] ;
-int index = 0;
-String cmd = "";
-void setup() {
+  // Turn off motors - Initial state
+  stopAll();
+}
+void loop()
+{
+  // directionControl();
+  // delay(10000);
+  speedControl();
+  delay(10000);
+}
+void stopAll()
+{
+  bottomLeft.stop();
+  bottomRight.stop();
+  topLeft.stop();
+  topLeft.stop();
+}
+//
+//void zeroSpeed()
+//{
+//   digitalWrite(bottomLeftEN, 0);
+//   digitalWrite(bottomRightEN, 0);
+//   digitalWrite(topLeftEN, 0);
+//   digitalWrite(topRightEN, 0);
+//}
 
-  Serial.begin(115200);
-  // put your setup code here, to run once:
-  AFMS.begin();
-  
-  LeftFrontWheel->setSpeed(defaultSpeed);
-  LeftBackWheel->setSpeed(defaultSpeed);
-  
-  RightBackWheel->setSpeed(defaultSpeed);
-  RightFrontWheel->setSpeed(defaultSpeed);
-
-  Bluetooth.begin(115200); // Default baud rate of the Bluetooth module
-  Bluetooth.setTimeout(1);
-  delay(20);
-  
+// This function lets you control spinning direction of motors
+void directionControl()
+{
+  // Set motors to maximum speed
+  // For PWM maximum possible values are 0 to 255
+  //   analogWrite(enA, 255);
+  //   analogWrite(enB, 255);
+  //
+  //   // Turn on motor A & B
+  //   digitalWrite(in1, HIGH);
+  //   digitalWrite(in2, LOW);
+  //   digitalWrite(in3, HIGH);
+  //   digitalWrite(in4, LOW);
+  //   delay(2000);
+  //
+  //   // Now change motor directions
+  //   digitalWrite(in1, LOW);
+  //   digitalWrite(in2, HIGH);
+  //   digitalWrite(in3, LOW);
+  //   digitalWrite(in4, HIGH);
+  //   delay(2000);
+  //
+  //   // Turn off motors
+  //   stopAll()
 }
 
-void loop() {
-  
-  // put your main code here, to run repeatedly:
-  if(Bluetooth.available())  // If the bluetooth sent any characters
+// This function lets you control speed of the motors
+void speedControl()
+{
+  // Turn on motors
+  bottomLeft.moveForward();
+  bottomRight.moveForward();
+  topLeft.moveForward();
+  topRight.moveForward();
+
+  // Accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++)
   {
-    while(Bluetooth.available()){
-      
-        message[index] = (char)Bluetooth.read();  // Add the character to the message buffer
-       // Serial.println(index);
-        index++;
-        if( index > MAXCHAR ){ 
-          index =0;
-          break;
-        }
-            
-    
-    }
-    Serial.println( message);
-    index = 0;
-    String msg = message;
-
-    clearMessage();
-
-    setSpeed();
-    
-    if(msg.equals("Forward")){
-        moveForward();
-    }
-    else if( msg.equals("Backward")){
-        moveBackward();
-    }
-    else if( msg.equals("SidewaysRight")){
-        moveSidewaysRight();
-    }
-    else if( msg.equals("SidewaysLeft")){
-        moveSidewaysLeft();
-    }
-    else if( msg.equals("RotateLeft")){
-       rotateLeft();
-    }
-    else if( msg.equals("RotateRight")){
-       rotateRight();
-    }
-    else if( msg.equals("RightForward")){
-        moveRightForward();
-    }
-    else if( msg.equals("LeftForward")){
-        moveLeftForward();
-    }
-    else if( msg.equals("Stop")){
-        motorStop();
-    }
-
-    delay(10);
-    
+    bottomLeft.setSpeed(i);
+    bottomRight.setSpeed(i);
+    topLeft.setSpeed(i);
+    topRight.setSpeed(i);
+    delay(30);
   }
-}
-void moveForward()
-{
-   LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(FORWARD);
-   
-   RightFrontWheel->run(FORWARD); 
-   RightBackWheel->run(FORWARD);
-  
-}
-void moveBackward() {
-  
-   LeftFrontWheel->run(BACKWARD);
-   LeftBackWheel->run(BACKWARD);
-   
-   RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(BACKWARD);
-   
-}
-void moveSidewaysRight() {
 
-   LeftFrontWheel->setSpeed(120);
-   LeftBackWheel->setSpeed(120);
-  
-   RightBackWheel->setSpeed(120);
-   RightFrontWheel->setSpeed(120);
+  // Decelerate from maximum speed to zero
+  for (int i = 255; i >= 0; --i)
+  {
+    bottomLeft.setSpeed(i);
+    bottomRight.setSpeed(i);
+    topLeft.setSpeed(i);
+    topRight.setSpeed(i);
+    delay(30);
+  }
 
-   LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(BACKWARD);
-
-   RightFrontWheel->run(FORWARD);
-   RightBackWheel->run(BACKWARD);
-   
-}
-void moveSidewaysLeft() {
-
-   LeftFrontWheel->setSpeed(120);
-   LeftBackWheel->setSpeed(100);
-
-   RightFrontWheel->setSpeed(100);
-   RightBackWheel->setSpeed(120);
-   
-  
-   LeftFrontWheel->run(BACKWARD);
-   LeftBackWheel->run(FORWARD);
-   
-   RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(FORWARD);
-   
-}
-void rotateLeft() {
-  
-   LeftFrontWheel->run(BACKWARD);
-   LeftBackWheel->run(BACKWARD);
-   
-   RightFrontWheel->run(FORWARD);
-   RightBackWheel->run(FORWARD);
-   
-}
-void rotateRight() {
-  
-   LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(FORWARD);
-   
-   RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(BACKWARD);
-   
-}
-void moveRightForward() {
-  
-   LeftFrontWheel->run(FORWARD);
-  // LeftBackWheel->run(FORWARD);
-   
- //  RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(BACKWARD);
-   
-}
-void moveRightBackward() {
-  
-  // LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(BACKWARD);
-   
-   RightFrontWheel->run(BACKWARD);
- //  RightBackWheel->run(BACKWARD);
-   
-}
-void moveLeftForward() {
-  // LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(FORWARD);
-   
-   RightFrontWheel->run(FORWARD);
- //  RightBackWheel->run(BACKWARD);
-}
-void moveLeftBackward() {
-  
-   LeftFrontWheel->run(BACKWARD);
-  // LeftBackWheel->run(FORWARD);
-   
- //  RightFrontWheel->run(FORWARD);
-   RightBackWheel->run(BACKWARD);
-}
-void motorStop(){
-  
-   LeftFrontWheel->run(RELEASE);
-   LeftBackWheel->run(RELEASE);
-   
-   RightFrontWheel->run(RELEASE);
-   RightBackWheel->run(RELEASE);
-}
-void clearMessage()
-{
-    for(int i=0 ;i< MAXCHAR; i++){
-       message[i] = (char)0;
-    }
-}
-void setSpeed()
-{
-   LeftFrontWheel->setSpeed(defaultSpeed);
-  LeftBackWheel->setSpeed(defaultSpeed);
-  
-  RightBackWheel->setSpeed(defaultSpeed);
-  RightFrontWheel->setSpeed(defaultSpeed);
+  // Now turn off motors
+  stopAll();
 }
